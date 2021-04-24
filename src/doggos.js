a4a856c75af63c3a28f6138393549272d8b623b4
Arda Sevinç <ardasevinc4@gmail.com>
2021-04-24 17:28:49 +0300

    Make things work

--- src/doggos.js
+++ src/doggos.js
@@ -1,26 +1,51 @@
 const DOG_URL = "https://dog.ceo/api/breeds/image/random";
 
+function loadDoge() {
+  let doge = document.querySelector(".doggo-pic");
+  let preloadEl = document.querySelector(".preload");
+
+  hide(preloadEl);
+  loaderOn();
+
+  const promise = fetch(DOG_URL);
+  promise
+    .then(function (response) {
+      const processingPromise = response.json();
+      return processingPromise;
+    })
+    .then(function (processedResponse) {
+      console.log(processedResponse);
+      doge.src = processedResponse.message;
+      loaderOff();
+    });
+}
+
+function hide(el) {
+  el.classList.add("nodisplay");
+}
+
+function unhide(el) {
+  el.classList.remove("nodisplay");
+}
+
+function loaderOn() {
+  let loader = document.querySelector(".loader");
+  let doge = document.querySelector(".doggo-pic");
+
+  hide(doge);
+  unhide(loader);
+}
+
+function loaderOff() {
+  let loader = document.querySelector(".loader");
+  let doge = document.querySelector(".doggo-pic");
+
+  hide(loader);
+  unhide(doge);
+}
+
 window.onload = function () {
-  let doggos = document.querySelector(".doggo");
   let button = document.querySelector(".spawn-btn");
-
-  function loadDoge() {
-    const promise = fetch(DOG_URL);
-    promise
-      .then(function (response) {
-        const processingPromise = response.json();
-        return processingPromise;
-      })
-      .then(function (processedResponse) {
-        console.log(processedResponse);
-
-        // const img = document.createElement("img");
-        // img.src = processedResponse.message;
-        // img.alt = "Cute doggo";
-        // doggos.append;
-        doggos.src = processedResponse.message;
-      });
-  }
 
   button.addEventListener("click", loadDoge, false);
 };
